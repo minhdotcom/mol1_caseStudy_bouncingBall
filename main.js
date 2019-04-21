1dc4eb33d57197def4c4275d7960586d3882629e
Minh <minhdotcom@gmail.com>
2019-04-21 15:20:26 +0700

    done

---
+++ main.js
@@ -0,0 +1,30 @@
+var context = canvas.getContext("2d"),
+    interval;
+    LOG = console.log;
+var tempBarX = 0,
+    tempBallX = 0;
+
+bar.setBarKeyBoard();
+
+function init_game () {
+    interval = setInterval(function () {
+        ball.move(bar.x, bar.x + bar.width);
+        bar.move();
+        if (bar.x == tempBarX) bar.accelaration = 0; else bar.accelaration ++;
+        if (bar.accelaration > 15) {bar.accelaration = 15;}
+        if (ball.bottomEdge >= bar.y) {
+            if((bar.x - tempBarX) / (ball.x - tempBallX) > 0) {
+                ball.moveAngle -= bar.accelaration / 50;
+            } else {
+                ball.moveAngle += bar.accelaration / 50;
+            }
+        }
+        tempBarX = bar.x;
+        tempBallX = ball.x;
+        for (let i = 0; i < bricks.length; i++) {
+            bricks[i].draw();
+        }
+    }, 100 - GAME_RESOLUTION);
+}
+
+init_game();
